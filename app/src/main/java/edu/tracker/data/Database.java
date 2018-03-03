fca415cbe5e5823e0b33a9c657e14fad649d3509
Tobi <tobi.chammings@gmail.com>
2018-03-03 17:05:56 +0000

    Start on database

---
+++ app/src/main/java/edu/tracker/data/Database.java
@@ -0,0 +1,24 @@
+package edu.tracker.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Database extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "data.db";
+
+    public Database(Context context) {
+        super(context, DB_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
